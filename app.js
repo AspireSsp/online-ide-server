6a6f3f0268b9d13d3eb5f534d78b93ddcd1d75ea
sanjay prajapat <sanjayprajapat68149@gmail.com>
2024-04-08 12:37:16 +0530

    scheduler added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
+const schedule = require('node-schedule');
 
 app.use(cors({
   origin: '*',
@@ -21,6 +22,7 @@ const user = require("./routes/userRoutes");
 const compiler = require("./routes/compiler");
 const codeFile = require("./routes/codeFile");
 const codeFolder = require("./routes/codeFolder");
+const { default: axios } = require("axios");
 
 app.use("/api/v1/compiler", compiler);
 app.use("/api/v1/user/", user);
@@ -30,4 +32,22 @@ app.use("/api/v1/folder/", codeFolder);
 app.get("/", (req, res)=>{
   res.status(200).json({success : true, message : "server is running ..."})
 })
+
+// Schedule for 3 AM
+const rule = new schedule.RecurrenceRule();
+rule.hour = 6;
+rule.minute = 0;
+
+const job = schedule.scheduleJob(rule, async function(){
+  try {
+    console.log('The world is going to end at 3 AM.');
+    const res = await axios.get('https://code-editer.onrender.com/');
+    console.log(res.data);
+  }
+  catch(error){
+    console.log("Error from morning schedule")
+  }
+});
+
+
 module.exports = app;
